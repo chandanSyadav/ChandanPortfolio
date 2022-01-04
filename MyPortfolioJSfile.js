3474cf5fabcf717fead34344436fa01bef49ac84
chandanSyadav <90094067+chandanSyadav@users.noreply.github.com>
2022-01-05 00:06:44 +0530

    first commit

---
+++ MyPortfolioJSfile.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+
+    // $(".ser1").click(function(){
+    //     $(".ser2").animate({
+    //         opacity:".4",    
+    //         // "margin-left":"200px",
+    //         width:"30px",
+    //         height:"50px",
+    //         fontSize:"2px",
+    //     },1000);
+    //     $(".ser3").animate({
+    //         opacity:".4",
+    //         // "margin-left":"200px",
+    //         width:"30px",
+    //         height:"50px",
+    //         fontSize:"2px",
+    //     },1000);
+    //     $("#abcd").animate({
+    //         fontSize:"0px"
+    //     });
+
+    // });
+
+    // $(".ser1").click(function(){
+    //     $(".ser2").fadeOut(1000);
+    //     $(".ser3").fadeOut(1000);
+    //     $(".ser1").animate({
+    //         height:"200px",
+    //         width:"100%",
+            
+    //         transition:"1s"
+    //     },100);
+    // });
+
+   
+});
+
+$(".services").owlCarousel({
+    margin: "100px",
+    loop: true,
+    autoplay:true,
+    autoplayTimeout:2000,
+});
+
